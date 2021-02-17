bbbb5fc7723707fbafa49d8c6b68e9043dc49258
Nachi Patil <npatil0821@gmail.com>
2021-02-17 17:34:30 -0500

    Added toast

--- app/src/main/java/edu/montclair/helloworld/MainActivity.java
+++ app/src/main/java/edu/montclair/helloworld/MainActivity.java
@@ -2,7 +2,10 @@ package edu.montclair.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//libraries imported for toast
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toast created
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        //toast displayed
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
